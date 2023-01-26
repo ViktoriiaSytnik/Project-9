90ac9edd457ca36642e4ab3e873f40109e02395a
Kristina <kristinaer304@gmail.com>
2023-01-26 17:15:45 +0200

    pie chart fixed

--- piechart.py
+++ piechart.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('../titles.csv')
+df = pd.read_csv('titles.csv')
 
 movie = df['title']
 ages = df['age_certification']
 map = {}
+
+
+nanAge = ages[4]
 
 for age in ages:
     if age in map:
@@ -13,11 +16,16 @@ for age in ages:
     else:
         map[age] = 1
 
+map.pop(nanAge)
+
+
+
+
 labels = list(map.keys())
 sizes = list(map.values())
 
 
-plt.pie(sizes,   labels=labels,shadow=True, startangle=90)
+plt.pie(sizes,  labels=labels,shadow=True, startangle=90)
 
 
 plt.show()
